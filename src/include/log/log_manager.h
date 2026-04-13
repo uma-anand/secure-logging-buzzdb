4f306593eaf9cb83166eb73c84ec1382113b4fbe
root <uma2005anand@gmail.com>
2026-04-13 19:51:23 +0000

    init repo

---
+++ src/include/log/log_manager.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <atomic>
+#include <map>
+#include <memory>
+#include <set>
+
+#include "buffer/buffer_manager.h"
+#include "storage/test_file.h"
+
+namespace buzzdb {
+
+class LogManager {
+   public:
+    enum class LogRecordType {
+        INVALID_RECORD_TYPE,
+        ABORT_RECORD,
+        COMMIT_RECORD,
+        UPDATE_RECORD,
+        BEGIN_RECORD,
+        CHECKPOINT_RECORD
+    };
+
+    /// Constructor.
+    LogManager(File* log_file);
+
+    /// Destructor.
+    ~LogManager();
+
+    /// Add an abort record
+    void log_abort(uint64_t txn_id, BufferManager& buffer_manager);
+
+    /// Add a commit record
+    void log_commit(uint64_t txn_id);
+
+    /// Add an update record
+    void log_update(uint64_t txn_id, uint64_t page_id, uint64_t length, uint64_t offset,
+                    std::byte* before_img, std::byte* after_img);
+
+    /// Add a txn begin record
+    void log_txn_begin(uint64_t txn_id);
+
+    /// Add a log checkpoint record
+    void log_checkpoint(BufferManager& buffer_manager);
+
+    /// recovery
+    void recovery(BufferManager& buffer_manager);
+
+    /// rollback a txn
+    void rollback_txn(uint64_t txn_id, BufferManager& buffer_manager);
+
+    /// Get log records
+    uint64_t get_total_log_records();
+
+    /// Get log records of a given type
+    uint64_t get_total_log_records_of_type(LogRecordType type);
+
+    /// reset the state, used to simulate crash
+    void reset(File* log_file);
+
+   private:
+    File* log_file_;
+
+    // offset in the file
+    size_t current_offset_ = 0;
+
+    std::map<uint64_t, uint64_t> txn_id_to_first_log_record;
+
+    std::map<LogRecordType, uint64_t> log_record_type_to_count;
+
+    std::set<uint64_t> active_txns;
+
+    template <typename T>
+    void write_val(T val) {
+        log_file_->write_block(reinterpret_cast<char*>(&val), current_offset_, sizeof(T));
+        current_offset_ += sizeof(T);
+    }
+
+    template <typename T>
+    T read_val(size_t& offset) {
+        T val;
+        log_file_->read_block(offset, sizeof(T), reinterpret_cast<char*>(&val));
+        offset += sizeof(T);
+        return val;
+    }
+};
+
+}  // namespace buzzdb
---
+++ src/log/log_manager.cc
@@ -0,0 +1,296 @@
+
+#include "log/log_manager.h"
+
+#include <string.h>
+
+#include <cassert>
+#include <cstddef>
+#include <iostream>
+
+#include "common/macros.h"
+#include "storage/test_file.h"
+
+namespace buzzdb {
+
+/**
+ * Functionality of the buffer manager that might be handy
+
+ Flush all the dirty pages to the disk
+        buffer_manager.flush_all_pages():
+
+ Write @data of @length at an @offset the buffer page @page_id
+        BufferFrame& frame = buffer_manager.fix_page(page_id, true);
+        memcpy(&frame.get_data()[offset], data, length);
+        buffer_manager.unfix_page(frame, true);
+
+ * Read and Write from/to the log_file
+   log_file_->read_block(offset, size, data);
+
+   Usage:
+   uint64_t txn_id;
+   log_file_->read_block(offset, sizeof(uint64_t), reinterpret_cast<char *>(&txn_id));
+   log_file_->write_block(reinterpret_cast<char *> (&txn_id), offset, sizeof(uint64_t));
+ */
+
+LogManager::LogManager(File* log_file) {
+    log_file_ = log_file;
+    log_record_type_to_count[LogRecordType::ABORT_RECORD] = 0;
+    log_record_type_to_count[LogRecordType::COMMIT_RECORD] = 0;
+    log_record_type_to_count[LogRecordType::UPDATE_RECORD] = 0;
+    log_record_type_to_count[LogRecordType::BEGIN_RECORD] = 0;
+    log_record_type_to_count[LogRecordType::CHECKPOINT_RECORD] = 0;
+}
+
+LogManager::~LogManager() {}
+
+void LogManager::reset(File* log_file) {
+    log_file_ = log_file;
+    current_offset_ = 0;
+    txn_id_to_first_log_record.clear();
+    log_record_type_to_count.clear();
+    active_txns.clear();
+}
+
+/// Get log records
+uint64_t LogManager::get_total_log_records() { 
+    uint64_t total = 0;
+    for (auto const& [type, count] : log_record_type_to_count) {
+        total += count;
+    }
+    return total; 
+}
+
+uint64_t LogManager::get_total_log_records_of_type(UNUSED_ATTRIBUTE LogRecordType type) {
+    return log_record_type_to_count[type];
+}
+
+/**
+ * Increment the ABORT_RECORD count.
+ * Rollback the provided transaction.
+ * Add abort log record to the log file.
+ * Remove from the active transactions.
+ */
+void LogManager::log_abort(UNUSED_ATTRIBUTE uint64_t txn_id,
+                           UNUSED_ATTRIBUTE BufferManager& buffer_manager) {
+    log_record_type_to_count[LogRecordType::ABORT_RECORD]++;
+    rollback_txn(txn_id, buffer_manager);
+    active_txns.erase(txn_id);
+    write_val(LogRecordType::ABORT_RECORD);
+    write_val(txn_id);
+}
+
+/**
+ * Increment the COMMIT_RECORD count
+ * Add commit log record to the log file
+ * Remove from the active transactions
+ */
+void LogManager::log_commit(UNUSED_ATTRIBUTE uint64_t txn_id) {
+    log_record_type_to_count[LogRecordType::COMMIT_RECORD]++;
+    active_txns.erase(txn_id);
+    write_val(LogRecordType::COMMIT_RECORD);
+    write_val(txn_id);
+}
+
+/**
+ * Increment the UPDATE_RECORD count
+ * Add the update log record to the log file
+ * @param txn_id		transaction id
+ * @param page_id		buffer page id
+ * @param length		length of the update tuple
+ * @param offset 		offset to the tuple in the buffer page
+ * @param before_img	before image of the buffer page at the given offset
+ * @param after_img		after image of the buffer page at the given offset
+ */
+void LogManager::log_update(UNUSED_ATTRIBUTE uint64_t txn_id, UNUSED_ATTRIBUTE uint64_t page_id,
+                            UNUSED_ATTRIBUTE uint64_t length, UNUSED_ATTRIBUTE uint64_t offset,
+                            UNUSED_ATTRIBUTE std::byte* before_img,
+                            UNUSED_ATTRIBUTE std::byte* after_img) {
+    log_record_type_to_count[LogRecordType::UPDATE_RECORD]++;
+
+    write_val(LogRecordType::UPDATE_RECORD);
+    write_val(txn_id);
+    write_val(page_id);
+    write_val(length);
+    write_val(offset);
+
+    log_file_->write_block(reinterpret_cast<char*>(before_img), current_offset_, length);
+    current_offset_ += length;
+
+    log_file_->write_block(reinterpret_cast<char*>(after_img), current_offset_, length);
+    current_offset_ += length;
+}
+
+/**
+ * Increment the BEGIN_RECORD count
+ * Add the begin log record to the log file
+ * Add to the active transactions
+ */
+void LogManager::log_txn_begin(UNUSED_ATTRIBUTE uint64_t txn_id) {
+    log_record_type_to_count[LogRecordType::BEGIN_RECORD]++;
+    active_txns.insert(txn_id);
+    if (txn_id_to_first_log_record.find(txn_id) == txn_id_to_first_log_record.end()) {
+        txn_id_to_first_log_record[txn_id] = current_offset_;
+    }
+    write_val(LogRecordType::BEGIN_RECORD);
+    write_val(txn_id);
+}
+
+/**
+ * Increment the CHECKPOINT_RECORD count
+ * Flush all dirty pages to the disk (USE: buffer_manager.flush_all_pages())
+ * Add the checkpoint log record to the log file
+ */
+void LogManager::log_checkpoint(UNUSED_ATTRIBUTE BufferManager& buffer_manager) {
+    log_record_type_to_count[LogRecordType::CHECKPOINT_RECORD]++;
+    buffer_manager.flush_all_pages();
+
+    write_val(LogRecordType::CHECKPOINT_RECORD);
+    
+    uint64_t active_count = active_txns.size();
+    write_val(active_count);
+
+    for (uint64_t txn_id : active_txns) {
+        write_val(txn_id);
+        write_val(txn_id_to_first_log_record[txn_id]);
+    }
+}
+
+/**
+ * @Analysis Phase:
+ * 		1. Get the active transactions and commited transactions
+ * 		2. Restore the txn_id_to_first_log_record
+ * @Redo Phase:
+ * 		1. Redo the entire log tape to restore the buffer page
+ * 		2. For UPDATE logs: write the after_img to the buffer page
+ * 		3. For ABORT logs: rollback the transactions
+ * 	@Undo Phase
+ * 		1. Rollback the transactions which are active and not commited
+ */
+void LogManager::recovery(UNUSED_ATTRIBUTE BufferManager& buffer_manager) {
+    size_t scan_offset = 0;
+    while (true) {
+        LogRecordType type = LogRecordType::INVALID_RECORD_TYPE;
+        log_file_->read_block(scan_offset, sizeof(LogRecordType), reinterpret_cast<char*>(&type));
+        if (type == LogRecordType::INVALID_RECORD_TYPE || type < LogRecordType::ABORT_RECORD || type > LogRecordType::CHECKPOINT_RECORD) {
+            break;
+        }
+        scan_offset += sizeof(LogRecordType);
+        if (type == LogRecordType::BEGIN_RECORD) {
+            uint64_t txn_id = read_val<uint64_t>(scan_offset);
+            active_txns.insert(txn_id);
+            if (txn_id_to_first_log_record.find(txn_id) == txn_id_to_first_log_record.end()) {
+                txn_id_to_first_log_record[txn_id] = scan_offset - sizeof(LogRecordType) - sizeof(uint64_t);
+            }
+        } 
+        else if (type == LogRecordType::COMMIT_RECORD) {
+            uint64_t txn_id = read_val<uint64_t>(scan_offset);
+            active_txns.erase(txn_id);
+        }
+        else if (type == LogRecordType::ABORT_RECORD) {
+            uint64_t txn_id = read_val<uint64_t>(scan_offset);
+            size_t temp_offset = current_offset_;
+            current_offset_ = scan_offset;
+            rollback_txn(txn_id, buffer_manager);
+            current_offset_ = temp_offset;
+            active_txns.erase(txn_id);
+        }
+        else if (type == LogRecordType::UPDATE_RECORD) {
+            read_val<uint64_t>(scan_offset); // move offset after trans id
+            uint64_t page_id = read_val<uint64_t>(scan_offset);
+            uint64_t length = read_val<uint64_t>(scan_offset);
+            uint64_t offset = read_val<uint64_t>(scan_offset);
+
+            std::vector<char> before_img(length);
+            log_file_->read_block(scan_offset, length, before_img.data());
+            scan_offset += length;
+
+            std::vector<char> after_img(length);
+            log_file_->read_block(scan_offset, length, after_img.data());
+            scan_offset += length;
+
+            BufferFrame& frame = buffer_manager.fix_page(page_id, true);
+            memcpy(&frame.get_data()[offset], after_img.data(), length);
+            buffer_manager.unfix_page(frame, true);
+        }
+        else if (type == LogRecordType::CHECKPOINT_RECORD) {
+            uint64_t active_count = read_val<uint64_t>(scan_offset);
+            for (uint64_t i = 0; i < active_count; ++i) {
+                uint64_t txn_id = read_val<uint64_t>(scan_offset);
+                uint64_t first_offset = read_val<uint64_t>(scan_offset);
+                active_txns.insert(txn_id);
+                txn_id_to_first_log_record[txn_id] = first_offset;
+            }
+        }
+    }
+    current_offset_ = scan_offset;
+    std::set<uint64_t> txns_to_undo = active_txns;
+    for (uint64_t txn_id : txns_to_undo) {
+        rollback_txn(txn_id, buffer_manager);
+    }
+}
+
+/**
+ * Use txn_id_to_first_log_record to get the begin of the current transaction
+ * Walk through the log tape and rollback the changes by writing the before
+ * image of the tuple on the buffer page.
+ * Note: There might be other transactions' log records interleaved, so be careful to
+ * only undo the changes corresponding to current transactions.
+ */
+void LogManager::rollback_txn(UNUSED_ATTRIBUTE uint64_t txn_id,
+                              UNUSED_ATTRIBUTE BufferManager& buffer_manager) {
+    if (txn_id_to_first_log_record.find(txn_id) == txn_id_to_first_log_record.end()) {
+        return; 
+    }
+
+    size_t scan_offset = txn_id_to_first_log_record[txn_id];
+
+    struct UpdateInfo {
+        uint64_t page_id;
+        uint64_t length;
+        uint64_t offset;
+        std::vector<char> before_img;
+    };
+    
+    std::vector<UpdateInfo> txn_updates;
+
+    while (scan_offset < current_offset_) {
+        LogRecordType type = LogRecordType::INVALID_RECORD_TYPE;
+        log_file_->read_block(scan_offset, sizeof(LogRecordType), reinterpret_cast<char*>(&type));
+        
+        if (type == LogRecordType::INVALID_RECORD_TYPE || type < LogRecordType::ABORT_RECORD || type > LogRecordType::CHECKPOINT_RECORD) {
+            break;
+        }
+        scan_offset += sizeof(LogRecordType);
+
+        if (type == LogRecordType::BEGIN_RECORD || type == LogRecordType::COMMIT_RECORD || type == LogRecordType::ABORT_RECORD) {
+            scan_offset += sizeof(uint64_t);
+        }
+        else if (type == LogRecordType::UPDATE_RECORD) {
+            uint64_t current_txn_id = read_val<uint64_t>(scan_offset);
+            uint64_t page_id = read_val<uint64_t>(scan_offset);
+            uint64_t length = read_val<uint64_t>(scan_offset);
+            uint64_t offset = read_val<uint64_t>(scan_offset);
+
+            std::vector<char> before_img(length);
+            log_file_->read_block(scan_offset, length, before_img.data());
+            scan_offset += length;
+
+            scan_offset += length; 
+
+            if (current_txn_id == txn_id) {
+                txn_updates.push_back({page_id, length, offset, before_img});
+            }
+        }
+        else if (type == LogRecordType::CHECKPOINT_RECORD) {
+            uint64_t active_count = read_val<uint64_t>(scan_offset);
+            scan_offset += active_count * (sizeof(uint64_t) * 2);
+        }
+    }
+    for (auto it = txn_updates.rbegin(); it != txn_updates.rend(); ++it) {
+        BufferFrame& frame = buffer_manager.fix_page(it->page_id, true);
+        memcpy(&frame.get_data()[it->offset], it->before_img.data(), it->length);
+        buffer_manager.unfix_page(frame, true);
+    }
+}
+
+}  // namespace buzzdb
